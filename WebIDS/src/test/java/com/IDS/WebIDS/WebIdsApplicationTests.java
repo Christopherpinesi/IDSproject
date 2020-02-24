363f4a505e78230fea6ee6ee08a49ac87d09b996
Christopherpinesi <60147548+Christopherpinesi@users.noreply.github.com>
2020-02-24 11:52:33 +0100

    Add files via upload

---
+++ WebIDS/src/test/java/com/IDS/WebIDS/WebIdsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.IDS.WebIDS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebIdsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
