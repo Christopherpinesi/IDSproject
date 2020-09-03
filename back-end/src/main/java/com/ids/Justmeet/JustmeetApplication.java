3343f9d9a8e88d881ce2a97fed065ef8e8c0fffb
Piccio <andrea.piccioni@studenti.unicam.it>
2020-09-03 23:06:33 +0200

    clean code and repository

---
+++ back-end/src/main/java/com/ids/Justmeet/JustmeetApplication.java
@@ -0,0 +1,13 @@
+package com.ids.Justmeet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustmeetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustmeetApplication.class, args);
+	}
+
+}
